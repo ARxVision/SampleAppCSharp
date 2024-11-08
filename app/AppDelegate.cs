a527ad475b6e6d26cb871987aa3c1a8ff581fbe8
Caroline <caranderson@microsoft.com>
2024-11-07 16:05:33 -0800

    connect

--- app/AppDelegate.cs
+++ app/AppDelegate.cs
@@ -11,19 +11,59 @@ public class AppDelegate : UIApplicationDelegate
 
     public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
     {
-        var viewModel = new ViewModel();
+        
         // create a new window instance based on the screen size
         Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
         // create a UIViewController with a single UILabel
         var vc = new UIViewController();
-        vc.View!.AddSubview(new UILabel(Window!.Frame)
+
+        var newLabel = new UILabel(new CGRect(50, 200, 250, 50))
         {
-            BackgroundColor = UIColor.SystemBackground,
+            Text = "Status: Disconnected",
             TextAlignment = UITextAlignment.Center,
-            Text = "Hello, iOS!",
-            AutoresizingMask = UIViewAutoresizing.All,
-        });
+            BackgroundColor = UIColor.SystemPink
+        };
+
+        // Create a UIImageView
+        var imageView = new UIImageView(new CGRect(50, 300, 200, 200))
+        {
+            Image = UIImage.FromFile("your_image.png"), // Replace with your image file
+            ContentMode = UIViewContentMode.ScaleAspectFit
+        };
+
+        // Add the new label to the view
+
+
+        var viewModel = new ViewModel(newLabel, imageView);
+
+        var disconnectButton = new UIButton(UIButtonType.System);
+        disconnectButton.Frame = new CGRect(50, 100, 100, 50);
+        disconnectButton.SetTitle("Disconnect", UIControlState.Normal);
+        disconnectButton.TouchUpInside += (sender, e) =>
+        {
+            // Handle disconnect action
+            Console.WriteLine("Disconnect button clicked");
+            viewModel.Disconnect();
+        };
+
+        // Create the Connect button
+        var connectButton = new UIButton(UIButtonType.System);
+        connectButton.Frame = new CGRect(200, 100, 100, 50);
+        connectButton.SetTitle("Connect", UIControlState.Normal);
+        connectButton.TouchUpInside += (sender, e) =>
+        {
+            viewModel.Connect();
+            // Handle connect action
+            Console.WriteLine("Connect button clicked");
+        };
+
+        // Add buttons to the view
+        vc.View.AddSubview(disconnectButton);
+        vc.View.AddSubview(connectButton);
+        vc.View.AddSubview(newLabel);
+        vc.View.AddSubview(imageView);
+
         Window.RootViewController = vc;
 
         // make the window visible
--- app/ViewModel.cs
+++ app/ViewModel.cs
@@ -5,11 +5,14 @@ using ObjCRuntime;
 public class ViewModel : NSObject, IArxHeadsetApi
 {
     private ArxHeadsetHandler _headsetHandler;
-
-    public ViewModel()
+    private UILabel label;
+    private UIImageView imageView;
+    public ViewModel(UILabel label, UIImageView image)
     {
         // Initialize ArxHeadsetHandler with the ViewModel as the delegate
         _headsetHandler = new ArxHeadsetHandler(this);
+        this.label = label;
+        this.imageView = image;
     }
 
     // Implement the IArxHeadsetApi methods here
@@ -18,28 +21,49 @@ public class ViewModel : NSObject, IArxHeadsetApi
         // Handle the connection error
     }
 
+    public void Connect()
+    {
+        this.label.Text = "Connecting";
+        _headsetHandler.Connect();
+    }
+
+    public void Disconnect()
+    {
+        this.label.Text = "Disconnecting";
+        _headsetHandler.Disconnect();
+    }
+
     public void OnDeviceConnectionErrorWithError(NSError error)
     {
         Console.Write("SeeingAI-Arx error is " + error.Description);
+        this.label.Text = error.Description;
     }
 
     public void OnDevicePhotoReceivedWithImage(UIImage image, Arx.Resolution frameDescriptor)
     {
+        //Console.WriteLine("SeeingAI-Arx photo received");
         // Handle the received photo
+        this.imageView.Image = image;
     }
 
     public void OnButtonClickedWithButton(ArxButton button, bool isLongPress)
     {
         // Handle the button click
+        this.label.Text = "Button clicked";
+        Console.WriteLine("SeeingAI-Arx button clicked");
     }
 
     public void OnDisconnect()
     {
+        Console.WriteLine("SeeingAI-Arx is disconnected");
+        this.label.Text = "disconnected";
         // Handle the disconnect
     }
 
     public void OnConnected()
     {
+        Console.WriteLine("SeeingAI-Arx is connected");
+        this.label.Text = "connected yay";
         // Handle the connected state
     }
 
